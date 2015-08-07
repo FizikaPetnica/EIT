b92faea82e74ca21e92b3283e93faaed8f045cf3
marija1 <masasindik@gmail.com>
2015-08-07 12:34:28 +0200

    Moment prelaza i koeficijent spontane emisije

---
+++ SEmission.m
@@ -0,0 +1,31 @@
+function A = SEmission (m1, s1, k1, m2, s2, k2)
+% odredjuje koeficijent spontane emisije
+
+% m je magnetni kvantni broj, s je za simetriju talasne f-je; trazenom
+% energetskom nivou odgovara k-ti minimum energija koji vraca Energy_levels
+
+g = 0;                              % jacina magnetnog polja
+R = 5;                              % poluprecnik kvantne tacke
+
+
+c = 137;                            %konstante u atomskim jedinicama
+hbar = 1;
+epsilon = 1/(4*pi);
+
+[~,e1,~] = Energy_levels (g, R, m1, s1);
+[~,e2,~] = Energy_levels (g, R, m2, s2);
+
+m = sort(sum(e1));                  %odredjivanje energija nivoa
+e1 = m(k1);
+m = sort(sum(e2));
+e2 = m(k2);
+
+omega = (e1-e2)/hbar;               %rezonantna frekvencija izmedju dva nivoa
+
+[dx,dy,dz] = Transition_dipole (m1, s1, k1, m2, s2, k2) ;
+d = dx^2+dy^2+dz^2;                 %kvadrat dipolnog momenta prelaza
+
+
+A = d*omega^3/(3*pi*epsilon*hbar*c^3);
+
+end
--- Transition_dipole.m
+++ Transition_dipole.m
@@ -1,35 +1,28 @@
-function [dx,dy,dz] = Transition_dipole   % odredjuje dipolni moment prelaza 
-                                          % za nivoe ciji se parametri unose rucno
+function [dx,dy,dz] = Transition_dipole (m1, s1, k1, m2, s2, k2) 
+% odredjuje dipolni moment prelaza 1->2
                                             
-k = 1;              % prvi nivo je k-ti minimum koji vraca Energy_levels
-l = 1;              % drugi nivo je l-ti minimum 
+% m je magnetni kvantni broj, s je za simetriju talasne f-je; trazenom
+% energetskom nivou odgovara k-ti minimum energija koji vraca Energy_levels
 
 g = 0;          % jacina magnetnog polja
 R = 5;          % poluprecnik kvantne tacke
 
 n = 25;         % broj podeoka, mora da bude isti i u Energy_levels da bi radilo 
 
-m1 = 0;
-m2 = 0;
-
-s1 = 1;
-s2 = -1;
 
 [g1,e1,~] = Energy_levels (g, R, m1, s1);
 [g2,e2,~] = Energy_levels (g, R, m2, s2);
 
 
-[~,i1] = sort(e1);          % nalazenje talasnih f-ja za prvi i drugi nivo
-index1 = i1(k);             % i njihova normalizacija
-g1 = g1(:,index1);
+[~,i] = sort(sum(e1));          % nalazenje talasnih f-ja za prvi i drugi nivo
+g1 = g1(:,i(k1));          % i njihova normalizacija
 g1 = vec2mat(g1,n);
 g1 = Normalized(g1,n,R);
 
-[~,i2] = sort(e2);
-index2 = i2(l);
-g2 = g2(:,index2);
+[~,i] = sort(sum(e2));
+g2 = g2(:,i(k2));
 g2 = vec2mat(g2,n);
-g1 = Normalized(g1,n,R);
+g2 = Normalized(g2,n,R);
 
 
 r = linspace(0,R,n+2) + R/(n+1);
@@ -52,5 +45,5 @@ dy = sumay*(1+s1*s2)*(abs(m2-m1)==1)*(m2-m1)*i*pi/n^2;
 dz = sumaz*(1-s1*s2)*(m2-m1==0)/n^2;
 
 end
-        
-        
+
+
