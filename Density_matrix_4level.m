41f59ea496a7f9ff131ce669ba60eb50b9528a62
marija1 <masasindik@gmail.com>
2015-08-12 15:14:19 +0200

    Razdvojeni realni i kompleksni delovi matrice gustine, radi za apsorpciju, gusnina naseljenosti ne radi

--- Density_matrix_4level.m
+++ Density_matrix_4level.m
@@ -1,78 +1,67 @@
-function  r = Density_matrix_4level 
+function ro = Density_matrix_4level(dp)
+% f-ja za odredjivanje matrice gustine za sistem od 4 nivoa
+% ulazni parametar je detuning za probe laser
 
 
-% syms ro11 ro21 ro22 ro31 ro32 ro33 ro41 ro42 ro43 ro44
+fi = 0;                     % fazna razlika
 
-dp = 0;
-
-g1 = 1;
+g1 = 1;                     % koef. spontane emisije
 g2 = 1;
 gp = 1;
 gc = 1;
 
-dc = 0;
+dc = 0;                     % detuning
 d1 = 0;
-d2 = 0;
+d2 = dp+dc-d1;
 
-G32 = (g1+gc+gp)/2+1i*dc;
-G34 = (g1+gc+g2)/2+1i*d1;
-G31 = (g1+gc)/2+1i*(dp+dc);
-G24 = (g2+gp)/2+1i*(dp-d2);
-G21 = gp/2+1i*dp;
-G41 = g2/2+1i*d2;
+G32 = (g1+gc+gp)/2 + 1i*dc;
+G34 = (g1+gc+g2)/2 + 1i*d1;
+G31 = (g1+gc)/2 + 1i*(dp+dc);
+G24 = (g2+gp)/2 + 1i*(dp-d2);
+G21 = gp/2 + 1i*dp;
+G41 = g2/2 + 1i*d2;
 
-fi = 0;
-
-rabyc = 10;
+rabyc = 10;                 % Rabijeve frekvencije
 rabyp = 0.05;
 raby1 = 10;
 raby2 = 1;
 
 
-syms ro11 ro21r ro21i ro22 ro31r ro31i ro32r ro32i ro33 ro41r ro41i ro42r ro42i ro43r ro43i ro44
+syms ro11 ro21real ro21imag ro22 ro31real ro31imag ro32real ro32imag ro33 ro41real ro41imag ro42real ro42imag ro43real ro43imag ro44
 
-
-e1 = -(g1+gc)*ro33 - 1i*rabyc*(ro32r+ro32i) - 1i*raby1*(ro43r-ro43i) + 1i*rabyc*(ro32r-ro32i) + 1i*raby1*(ro43r+ro43i) == 0;
-e2 = gc*ro33 - gp*ro22 + 1i*rabyc*(ro32r+ro32i) - 1i*rabyp*(ro21r+ro21i) - 1i*rabyc*(ro32r-ro32i) + 1i*rabyp*(ro21r-ro21i) == 0;
-e3 = g1*ro33 - g2*ro44 + 1i*raby1*(ro43r-ro43i) - 1i*raby2*(ro41r+ro41i) - 1i*raby1*(ro43r+ro43i) + 1i*raby2*(ro41r-ro41i) == 0;
-e4 = -G32*(ro32r+ro32i) - 1i*rabyc*(ro33-ro22) - 1i*rabyp*(ro31r+ro31i)*exp(-1i*fi) + 1i*raby1*(ro42r+ro42i) == 0;
-e5 = -G34*(ro43r-ro43i) - 1i*raby1*(ro33-ro44) - 1i*raby2*(ro31r+ro31i) + 1i*rabyc*(ro42r-ro42i) + 1i*rabyp*(ro21r-ro21i) == 0;
-e6 = -G31*(ro31r+ro31i) - 1i*rabyp*(ro32r+ro32i)*exp(-1i*fi) - 1i*raby2*(ro43r-ro43i) + 1i*rabyc*(ro21r+ro21i)*exp(-1i*fi) + 1i*raby1*(ro41r+ro41i) == 0;
-e7 = -G24*(ro42r-ro42i) + 1i*rabyc*(ro43r-ro43i) - 1i*raby2*(ro21r+ro21i)*exp(-1i*fi) - 1i*raby1*(ro32r-ro32i) + 1i*rabyp*(ro41r-ro41i)*exp(-1i*fi) == 0;
-e8 = -G21*(ro21r+ro21i) - 1i*rabyp*(ro22-ro11) + 1i*rabyc*(ro31r+ro31i)*exp(1i*fi) - 1i*raby2*(ro42r-ro42i)*exp(1i*fi) == 0;
-e9 = -G41*(ro41r+ro41i) - 1i*raby2*(ro44-ro11) + 1i*raby1*(ro31r+ro31i) - 1i*rabyp*(ro42r+ro42i)*exp(-1i*fi) == 0;
+e1 = -(g1 + gc)*ro33 - 1i*rabyc*(ro32real+1i*ro32imag) - 1i*raby1*(ro43real-1i*ro43imag) + 1i*rabyc*(ro32real-1i*ro32imag) + 1i*raby1*(ro43real+1i*ro43imag) == 0; 
+e2 = gc*ro33 - gp*ro22 + 1i*rabyc*(ro32real+1i*ro32imag) - 1i*rabyp*(ro21real+1i*ro21imag) - 1i*rabyc*(ro32real-1i*ro32imag) + 1i*rabyp*(ro21real-1i*ro21imag) == 0; 
+e3 = g1*ro33 - g2*ro44 + 1i*raby1*(ro43real-1i*ro43imag) - 1i*raby2*(ro41real+1i*ro41imag) - 1i*raby1*(ro43real+1i*ro43imag) + 1i*raby2*(ro41real-1i*ro41imag) == 0;
+e4 = -G32*(ro32real+1i*ro32imag) - 1i*rabyc*(ro33 - ro22) - 1i*rabyp*(ro31real+1i*ro31imag)*exp(-1i*fi) + 1i*raby1*(ro42real+1i*ro42imag) == 0;
+e5 = -G34*(ro43real-1i*ro43imag) - 1i*raby1*(ro33 - ro44) - 1i*raby2*(ro31real+1i*ro31imag) + 1i*rabyc*(ro42real-1i*ro42imag) + 1i*rabyp*(ro21real-1i*ro21imag) == 0;
+e6 = -G31*(ro31real+1i*ro31imag) - 1i*rabyp*(ro32real+1i*ro32imag)*exp(-1i*fi) - 1i*raby2*(ro43real-1i*ro43imag) + 1i*rabyc*(ro21real+1i*ro21imag)*exp(-1i*fi) + 1i*raby1*(ro41real+1i*ro41imag) == 0;
+e7 = -G24*(ro42real-1i*ro42imag) + 1i*rabyc*(ro43real-1i*ro43imag) - 1i*raby2*(ro21real+1i*ro21imag)*exp(-1i*fi) - 1i*raby1*(ro32real-1i*ro32imag) + 1i*rabyp*(ro41real-1i*ro41imag)*exp(-1i*fi) == 0;
+e8 = -G21*(ro21real+1i*ro21imag) - 1i*rabyp*(ro22 - ro11) + 1i*rabyc*(ro31real+1i*ro31imag)*exp(1i*fi) - 1i*raby2*(ro42real-1i*ro42imag)*exp(1i*fi) == 0;
+e9 = -G41*(ro41real+1i*ro41imag) - 1i*raby2*(ro44 - ro11) + 1i*raby1*(ro31real+1i*ro31imag) - 1i*rabyp*(ro42real+1i*ro42imag)*exp(-1i*fi) == 0;
 e10 = ro11 + ro22 + ro33 + ro44 == 1;
-% e11 = ro12 == conj(ro21);
-% e12 = ro13 == conj(ro31);
-% e13 = ro14 == conj(ro41);
-% e14 = ro23 == conj(ro32);
-% e15 = ro24 == conj(ro42);
-% e16 = ro34 == conj(ro43);
-
-r = solve([e1, e2, e3, e4, e5, e6, e7, e8, e9, e10] ,...
-    [ro11 ro21r ro21i ro22 ro31r ro31i ro32r ro32i ro33 ro41r ro41i ro42r ro42i ro43r ro43i ro44]);
 
 
+r = solve([e1,e2,e3,e4,e5,e6,e7,e8,e9,e10],...
+    [ro11, ro21real, ro21imag, ro22 ro31real ro31imag ro32real ro32imag ro33 ro41real ro41imag ro42real ro42imag ro43real ro43imag ro44]);
 
-% e1 = -(g1+gc)*ro33 - 1i*rabyc*ro32 - 1i*raby1*conj(ro43) + 1i*rabyc*conj(ro32) + 1i*raby1*ro43 == 0;
-% e2 = gc*ro33 - gp*ro22 + 1i*rabyc*ro32 - 1i*rabyp*ro21 - 1i*rabyc*conj(ro32) + 1i*rabyp*conj(ro21) == 0;
-% e3 = g1*ro33 - g2*ro44 + 1i*raby1*conj(ro43) - 1i*raby2*ro41 - 1i*raby1*ro43 + 1i*raby2*conj(ro41) == 0;
-% e4 = -G32*ro32 - 1i*rabyc*(ro33-ro22) - 1i*rabyp*ro31*exp(-1i*fi) + 1i*raby1*ro42 == 0;
-% e5 = -G34*conj(ro43) - 1i*raby1*(ro33-ro44) - 1i*raby2*ro31 + 1i*rabyc*conj(ro42) + 1i*rabyp*conj(ro21) == 0;
-% e6 = -G31*ro31 - 1i*rabyp*ro32*exp(-1i*fi) - 1i*raby2*conj(ro43) + 1i*rabyc*ro21*exp(-1i*fi) + 1i*raby1*ro41 == 0;
-% e7 = -G24*conj(ro42) + 1i*rabyc*conj(ro43) - 1i*raby2*ro21*exp(-1i*fi) - 1i*raby1*conj(ro32) + 1i*rabyp*conj(ro41)*exp(-1i*fi) == 0;
-% e8 = -G21*ro21 - 1i*rabyp*(ro22-ro11) + 1i*rabyc*ro31*exp(1i*fi) - 1i*raby2*conj(ro42)*exp(1i*fi) == 0;
-% e9 = -G41*ro41 - 1i*raby2*(ro44-ro11) + 1i*raby1*ro31 - 1i*rabyp*ro42*exp(-1i*fi) == 0;
-% e10 = ro11 + ro22 + ro33 + ro44 == 1;
-% 
-% r = solve([e1, e2, e3, e4, e5, e6, e7, e8, e9, e10] , [ro11, ro21, ro22, ro31, ro32, ro33, ro41, ro42, ro43, ro44]);
+ro = zeros(4,4);                                        % density matrix
 
-% ro(1,2) = conj(ro(2,1));
-% ro(1,3) = conj(ro(3,1));
-% ro(1,4) = conj(ro(4,1));
-% ro(2,3) = conj(ro(3,2));
-% ro(2,4) = conj(ro(4,2));
-% ro(3,4) = conj(ro(4,3));
+ro(1,1) = r.ro11;
+ro(2,1) = r.ro21real + 1i*r.ro21imag;
+ro(2,2) = r.ro22;
+ro(3,1) = r.ro31real + 1i*r.ro31imag;
+ro(3,2) = r.ro32real + 1i*r.ro32imag;
+ro(3,3) = r.ro33;
+ro(4,1) = r.ro41real + 1i*r.ro41imag;
+ro(4,2) = r.ro42real + 1i*r.ro42imag;
+ro(4,3) = r.ro43real + 1i*r.ro43imag;
+ro(4,4) = r.ro44;
 
+ro(1,2) = conj(ro(2,1));
+ro(1,3) = conj(ro(3,1));
+ro(1,4) = conj(ro(4,1));
+ro(2,3) = conj(ro(3,2));
+ro(2,4) = conj(ro(4,2));
+ro(3,4) = conj(ro(4,3));
 
 end
